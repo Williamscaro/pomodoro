a054e922ddf3e3a88816eef0f1bb889d9af7ec25
Williams Caro <williamscaro194@gmail.com>
2021-09-23 17:15:31 -0400

    TImer Realizardo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import SetPomodoro from "./components/SetPomodoro";
+
 function App() {
   return (
     <div className="container">
         <h1> pomodoro</h1>
         <small> Ser Productivo, es el camino </small>
+        <SetPomodoro />
     </div>
   );
 }
